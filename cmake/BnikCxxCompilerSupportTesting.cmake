164dcea91d9614a38fb1d2c1a05024e47991b510
ndgloc9 <111042968+indigo-loc99@users.noreply.github.com>
2024-07-05 23:38:38 -0700

    Include CMake try_compile test code in source tree

--- cmake/BnikCxxCompilerSupportTesting.cmake
+++ cmake/BnikCxxCompilerSupportTesting.cmake
@@ -16,13 +16,8 @@ if(BNIK_USE_CMAKE_CXX_MODULE_STD)
                 SOURCE_FROM_CONTENT "has_cxx_std_module.cpp" "import std;\nint main() {\n    auto arr = std::array{0, 2, 2};\n    return arr[0];\n}\n"
                 CMAKE_FLAGS -DCMAKE_EXPERIMENTAL_CXX_IMPORT_STD:STRING=${CMAKE_EXPERIMENTAL_CXX_IMPORT_STD} -DCMAKE_CXX_MODULE_STD:BOOL=ON -DCMAKE_CXX_SCAN_FOR_MODULES:BOOL=ON
                 CXX_STANDARD 23
-                OUTPUT_VARIABLE _OUT_LOG
              )
 
-  message(WARNING "Result of BNIK_HAS_CXX_MODULE_STD is '${BNIK_HAS_CXX_MODULE_STD}'")
-  if(NOT BNIK_HAS_CXX_MODULE_STD)
-    #message(WARNING "Try-Compile test for 'BNIK_HAS_CXX_MODULE_STD' has something to say:\n${_OUT_LOG}\n")
-  endif()
 endif()
 
 if(BNIK_ENABLE_BNIK_CXX_MODULES)
@@ -33,24 +28,19 @@ if(BNIK_ENABLE_BNIK_CXX_MODULES)
   if(CMAKE_CXX_COMPILER_ID MATCHES "^[Mm][Ss][Vv][Cc]$")
     set(_FILE_EXTENSION "ixx")
   else()
-    set(_FILE_EXTENSION "cpp")
+    set(_FILE_EXTENSION "cppm")
   endif()
 
   try_compile(BNIK_CAN_BUILD_AS_CXX_MODULES
                 SOURCES_TYPE CXX_MODULE
-                SOURCE_FROM_CONTENT "can_build_cxx_modules.${_FILE_EXTENSION}" "module;\nexport module FooModule;\n"
+                SOURCES "${CMAKE_SOURCE_DIR}/cmake/trycompile/can_build_cxx_modules.${_FILE_EXTENSION}"
 
                 SOURCES_TYPE NORMAL
-                SOURCE_FROM_CONTENT "main.cpp" "import FooModule;\nint main() {}"
+                SOURCE_FROM_CONTENT "main.cpp" "import FooModule;\nint main() {int v = foo::add(2,2);}"
 
                 #CMAKE_FLAGS -DCMAKE_CXX_SCAN_FOR_MODULES:BOOL=ON
                 CXX_STANDARD 23
-                OUTPUT_VARIABLE _OUT_LOG
   )
-  endblock()
 
-  message(WARNING "Result of BNIK_CAN_BUILD_AS_CXX_MODULES is '${BNIK_CAN_BUILD_AS_CXX_MODULES}'")
-  if(NOT BNIK_CAN_BUILD_AS_CXX_MODULES)
-    message(WARNING "Try-Compile test for 'BNIK_CAN_BUILD_AS_CXX_MODULES' has something to say:\n${_OUT_LOG}\n")
-  endif()
+  endblock()
 endif()
---
+++ cmake/trycompile/can_build_cxx_modules.ixx
@@ -0,0 +1,10 @@
+module;
+export module FooModule;
+
+export namespace foo
+{
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+}
